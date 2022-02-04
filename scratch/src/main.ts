76f14150287b2d2def15c00e3412b044cf34c1b6
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-04 08:55:46 +0100

    refactor: moved app module and controller to their own files

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,23 +1,5 @@
-// Normally the module and the controller are in separate files,
-// but for now we'll create the module and the controller inside the main file.addEventListener('
-import { Controller, Module, Get } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-@Controller()
-class AppController {
-  @Get()
-  getRootRoute() {
-    return 'hi there!';
-  }
-}
-
-// Whenever our app starts up, nest is going to look at this app module,
-// it's going to find all the controllers listed at `controllers` property
-// and it's going to automatically create an instance of all our different controller classes
-@Module({
-  controllers: [AppController]
-})
-class AppModule {}
+import { AppModule } from './app.module';
 
 async function bootstrap () {
   const app = await NestFactory.create(AppModule);
