e513dfba1460ea4938546b95f434d6b6eeb8b9c8
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-04 08:42:42 +0100

    feat: added AppModule

--- scratch/src/main.ts
+++ scratch/src/main.ts
@@ -1,6 +1,7 @@
 // Normally the module and the controller are in separate files,
 // but for now we'll create the module and the controller inside the main file.addEventListener('
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Module, Get } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 @Controller()
 class AppController {
@@ -9,3 +10,11 @@ class AppController {
     return 'hi there!';
   }
 }
+
+// Whenever our app starts up, nest is going to look at this app module,
+// it's going to find all the controllers listed at `controllers` property
+// and it's going to automatically create an instance of all our different controller classes
+@Module({
+  controllers: [AppController]
+})
+class AppModule {}
