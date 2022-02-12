9324bfd793c6d33765535170c59b1c94137f4ec5
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-12 11:47:59 +0100

    feat: connected messages controller to messages service

--- 02_messages/src/messages/messages.controller.ts
+++ 02_messages/src/messages/messages.controller.ts
@@ -1,21 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
+import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('/messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // Controller is creating its own dependencies
+    // DON'T DO THIS ON A REAL APP!
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
-
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log('body :>> ', body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log('id :>> ', id);
+    return this.messagesService.findOne(id);
   }
 }
