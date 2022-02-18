a84dd7ffdbed3c3895b4d21acfb58314d2a7cdbd
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-18 08:53:17 +0100

    feat: using dependency injection between modules

--- 03_di/src/power/power.module.ts
+++ 03_di/src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports:[PowerService] // by default the service is private to the module, so we need to export it so it can be used in other modules
 })
 export class PowerModule {}
