0cbf66bfdec089827d9a7eb0e73e04aa56946cb7
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-21 08:23:04 +0100

    feat: added anc connected Report entity

--- 04_my-car-value/src/reports/reports.module.ts
+++ 04_my-car-value/src/reports/reports.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.entity';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Report])], // this will create the Report repository for us
   controllers: [ReportsController],
   providers: [ReportsService]
 })
