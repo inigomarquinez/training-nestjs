cc99f5b656591d280f313e6caf79904d95881346
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-22 08:36:55 +0100

    feat: added validation pipe globally

--- 04_my-car-value/src/main.ts
+++ 04_my-car-value/src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // make sure that incoming requests don't have extraneous properties in the body that we're not expecting
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
