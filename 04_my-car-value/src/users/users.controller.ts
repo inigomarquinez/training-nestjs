8d321612f88e6baee390b5abcf163d901579640e
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-23 08:37:36 +0100

    feat: added users controller logic

--- 04_my-car-value/src/users/users.controller.ts
+++ 04_my-car-value/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -7,7 +8,27 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post('/signup')
-  async createUser(@Body() body: CreateUserDto): Promise<any> {
+  createUser(@Body() body: CreateUserDto) {
     this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) { // every single part of the URL is a string! We'll need to parse the id into a number
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
