29c905e41dc973e84e3311c63d14f79044963b93
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-26 12:39:32 +0100

    feat: added new AuthService

--- 04_my-car-value/src/users/users.module.ts
+++ 04_my-car-value/src/users/users.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -7,6 +9,6 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // this will create the User repository for us
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [AuthService, UsersService]
 })
 export class UsersModule {}
