15a0f36f2ea35243b78ee8d230ade52ce3cc6036
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-02-26 14:17:45 +0100

    feat: setting up sessions

--- 04_my-car-value/src/main.ts
+++ 04_my-car-value/src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+
+const cookieSession = require('cookie-session'); // We need to do a requre due to our tsconfig
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['ajhakhahsaj'] // used to encrypt the cookie
+  }));
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // make sure that incoming requests don't have extraneous properties in the body that we're not expecting
