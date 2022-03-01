ca6694585219f77b8ccde7d851f18d9ffe36b24f
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-03-01 18:26:10 +0100

    test: improving test files layout

--- 04_my-car-value/src/users/auth.service.spec.ts
+++ 04_my-car-value/src/users/auth.service.spec.ts
@@ -3,27 +3,34 @@ import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service (so we can very easily control the behaviour)
-  // We're only creating find and create because the auth service only uses those methods inside users service
-  const fakeUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
-  };
+let service: AuthService;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      // This is how we inject the fake users service:
-      // If anyone asks for UsersService, we'll return the fakeUserService
-      {
-        provide: UsersService,
-        useValue: fakeUserService
-      }
-    ]
-  }).compile();
+describe('AuthService', () => {
+  beforeEach(async () => {
+    // Create a fake copy of the users service (so we can very easily control the behaviour)
+    // We're only creating find and create because the auth service only uses those methods inside users service
+    const fakeUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        // This is how we inject the fake users service:
+        // If anyone asks for UsersService, we'll return the fakeUserService
+        {
+          provide: UsersService,
+          useValue: fakeUserService
+        }
+      ]
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
 })
