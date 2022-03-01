245d5f2a8c57cab19114e006b157f264f8fba192
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-03-01 08:29:17 +0100

    test: getting TypeScript to Help With Mocks

--- 04_my-car-value/src/users/auth.service.spec.ts
+++ 04_my-car-value/src/users/auth.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
   // Create a fake copy of the users service (so we can very easily control the behaviour)
-  const fakeUserService = {
+  // We're only creating find and create because the auth service only uses those methods inside users service
+  const fakeUserService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    create: (email: string, password: string) => Promise.resolve({ id: 1, email, password }),
+    create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
