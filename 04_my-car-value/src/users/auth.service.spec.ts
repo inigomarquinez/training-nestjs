89e19096aa86983e3cb063761e8aa2314e1a1711
Íñigo Marquínez Prado <inigo.marquinez@guidesmiths.com>
2022-03-01 18:47:21 +0100

    test: changing mock implementation

--- 04_my-car-value/src/users/auth.service.spec.ts
+++ 04_my-car-value/src/users/auth.service.spec.ts
@@ -4,13 +4,15 @@ import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { async } from 'rxjs';
 
-let service: AuthService;
 
 describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
   beforeEach(async () => {
     // Create a fake copy of the users service (so we can very easily control the behaviour)
     // We're only creating find and create because the auth service only uses those methods inside users service
-    const fakeUserService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User),
     };
@@ -22,7 +24,7 @@ describe('AuthService', () => {
         // If anyone asks for UsersService, we'll return the fakeUserService
         {
           provide: UsersService,
-          useValue: fakeUserService
+          useValue: fakeUsersService
         }
       ]
     }).compile();
@@ -41,5 +43,25 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with an email that is in use', (done) => {
+    // Redefining a method of the mocked users service so we can test for the error
+    fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'test@test.com', password: 'password' } as User]);
+
+
+    // This format combined with async doesn't work from jest v27 that uses jest-circus (Test functions cannot both take a 'done' callback and return something. Either use a 'done' callback, or return a promise.)
+    // try {
+    //   await service.signup('test@test.com', 'password');
+    // } catch (error) {
+    //   done();
+    // }
+
+    // This is the correct way to do it:
+    service
+      .signup('test@test.com', 'password')
+      .catch(_error => {});
+
+    done();
   });
 })
